9b6b1629114b08726990809296270f95bb06df9d
Luis Felipe Ojeda Narvaez <felipe4k@outlook.es>
2022-07-04 16:18:46 -0500

    suma de variables numericas en java

---
+++ suma.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class suma {
+    public static void main(String[] args) {
+        //Float a,b,c;
+        System.out.println("digite tres numeros");
+        Scanner entradas = new Scanner(System.in);
+        float a=entradas.nextFloat();
+        float b=entradas.nextFloat();
+        float c=entradas.nextFloat();// usar comas 
+        System.out.println("the suma is "+(a+b+c));
+
+    }
+
+
+    
+}
