ef7d9ebc1817cc21ab480260b6bb28728c2ce59f
Luis Felipe Ojeda Narvaez <felipe4k@outlook.es>
2022-07-04 16:25:51 -0500

    incremento para ahorrar codigo y mayor comprension operadores

---
+++ Incremento.java
@@ -0,0 +1,17 @@
+public class Incremento {
+    public static void main(String[] args) {
+       // int x=5;
+       // x+=1;// aumenta a 6
+        // x++; // incrementa en uno el valor de x
+        // x--;// decrementa en uno seria 6
+
+        int x=5, y;
+        y=++x; // el orden importa prefijo primero hace el aumento o decremento 
+
+
+        System.out.println(y); //por poner de sufijo    ++ aumento 
+        System.out.println(x); // incrementa depues el valor de x
+    }
+
+    
+}
